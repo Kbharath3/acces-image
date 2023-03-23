7482ba3d723e05d684fde1ddbd0a7ee6fbd556a2
K B <k.bharath@springml.com>
2023-03-23 21:26:10 +0530

    modified angular firebase

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
 import { LastComponent } from './last/last.component';
 import{ FormsModule } from '@angular/forms';
-import { SignupComponent } from './signup/signup.component'
+import { SignupComponent } from './signup/signup.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore'
 
 @NgModule({
   declarations: [
@@ -31,7 +33,8 @@ import { SignupComponent } from './signup/signup.component'
     FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -4,15 +4,16 @@
 
 export const environment = {
   firebase: {
-    projectId: 'bharath-363610',
-    appId: '1:790621399626:web:a1841fe6679616866d14b3',
-    storageBucket: 'bharath-363610.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyDZbRC7Ei78JbBjEJtw_QYb8NNoyRdP6e0',
-    authDomain: 'bharath-363610.firebaseapp.com',
-    messagingSenderId: '790621399626',
+    projectId: 'bharath-381509',
+    appId: '1:81886726412:web:fc8b2bcf336108fd7de129',
+    storageBucket: 'bharath-381509.appspot.com',
+    apiKey: 'AIzaSyCdxonQmZDMTlPOu5PVYb78R-Elle9Pmvs',
+    authDomain: 'bharath-381509.firebaseapp.com',
+    messagingSenderId: '81886726412',
+    measurementId: 'G-FK2R0WNZ8F',
   },
-  production: false
+ 
+  
 };
 
 /*
